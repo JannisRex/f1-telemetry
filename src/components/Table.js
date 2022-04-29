87a9c09a679b2dd8c2889da53590ac93c081b7fd
JannisRex <rex.jannis@web.de>
2022-04-30 01:05:07 +0200

    [initialized react app]
    Signed-off-by: JannisRex <rex.jannis@web.de>

---
+++ src/components/Table.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import '../config/styles.css'
+
+export default class Table extends Component {
+  render() {
+    return (
+      <div className="wrapper">
+        
+      </div>
+    )
+  }
+}
