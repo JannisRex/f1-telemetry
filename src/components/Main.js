3aae18593ad8807e0a04deaed714e3516437276f
JannisRex <rex.jannis@web.de>
2022-04-30 03:00:50 +0200

    boilerplate
    Signed-off-by: JannisRex <rex.jannis@web.de>

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react'
 import { w3cwebsocket as W3CWebSocket } from 'websocket'
 
-const client = new W3CWebSocket('ws://127.0.0.1:8000')
-
+const client = new W3CWebSocket('ws://127.0.0.1:20778')
 
 // Will get Data from Websocket -> Pass it to Components
 // -> Then Render Data
 class Main extends Component {
-  componentWillMount() {
+  componentDidMount() {
     client.onopen = () => {
       console.log('WebSocket Client Connected')
     }
