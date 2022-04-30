7c564d5cbb8c7ea9853c60bae2d6f69331a47f74
JannisRex <rex.jannis@web.de>
2022-04-30 14:10:53 +0200

    [add] basic 3split layout for main-page
    Signed-off-by: JannisRex <rex.jannis@web.de>

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,22 +1,34 @@
 import React, { Component } from 'react'
-import { w3cwebsocket as W3CWebSocket } from 'websocket'
 
-const client = new W3CWebSocket('ws://127.0.0.1:20778')
+// import { w3cwebsocket as W3CWebSocket } from 'websocket'
+// const client = new W3CWebSocket('ws://127.0.0.1:20778')
 
 // Will get Data from Websocket -> Pass it to Components
 // -> Then Render Data
 class Main extends Component {
-  componentDidMount() {
-    client.onopen = () => {
-      console.log('WebSocket Client Connected')
-    }
-    client.onmessage = (message) => {
-      console.log(message)
-    }
-  }
+  // componentDidMount() {
+  //   client.onopen = () => {
+  //     console.log('WebSocket Client Connected')
+  //   }
+  //   client.onmessage = (message) => {
+  //     console.log(message)
+  //   }
+  // }
 
   render() {
-    return <div>WebSocket</div>
+    return (
+      <div class='container'>
+        <div class='section red'>
+          <div className='wrapper'>LEFT CONTENT</div>
+        </div>
+        <div class='section blue'>
+          <div className='wrapper'>CENTER CONTENT</div>
+        </div>
+        <div class='section green'>
+          <div className='wrapper'>RIGHT CONTENT</div>
+        </div>
+      </div>
+    )
   }
 }
 
