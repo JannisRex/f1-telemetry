870e7241802e79083127c2f1045fe9c26673e505
JannisRex <rex.jannis@web.de>
2022-04-30 02:29:43 +0200

    [add] boilerplate for websocket
    Signed-off-by: JannisRex <rex.jannis@web.de>

---
+++ src/components/Main.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+import { w3cwebsocket as W3CWebSocket } from 'websocket'
+
+const client = new W3CWebSocket('ws://127.0.0.1:8000')
+
+
+// Will get Data from Websocket -> Pass it to Components
+// -> Then Render Data
+class Main extends Component {
+  componentWillMount() {
+    client.onopen = () => {
+      console.log('WebSocket Client Connected')
+    }
+    client.onmessage = (message) => {
+      console.log(message)
+    }
+  }
+
+  render() {
+    return <div>WebSocket</div>
+  }
+}
+
+export default Main
